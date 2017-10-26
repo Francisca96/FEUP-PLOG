25f2ef11ff11ec6b727b8aa85dadcfe925eca58d
Francisca96 <franciscapauperio@gmail.com>
2017-10-26 18:29:47 +0100

    change all board display

--- Interface.pl
+++ Interface.pl
@@ -1,12 +1,12 @@
 show_piece(p(Piece, Position)):-
-  Piece == null ->
+  Piece == 0 ->
   symbol(Position, Val),
   write(Val);
   symbol(Piece, Val),
   write(Val).
 
 
-display_board([L|T]):-
+display_board(Board):-
   write('________________________________________________'),
   nl,
   write('|                                               |'),
@@ -14,210 +14,232 @@ display_board([L|T]):-
   write('|                                               |'),
   nl,
   write('|             '),
-  display_elems_line_1(L,T).
+  display_elems_line_1_13(Board, 1).
+
 display_board([]).
 
-display_elems_line_1([S|E],T):-
+display_elems_line_1_13([S|E], Counter):-
+  Counter =< 4,
+  Counter1 is Counter+1,
   show_piece(S),
   write('    '),
-  display_elems_line_1(E, T).
-display_elems_line_1([], T):-
+  display_elems_line_1_13(E, Counter1).
+display_elems_line_1_13(E, 5):-
   write('          |'),
   nl,
   write('|                                               |'),
   nl,
-  display_line_2(T).
+  display_line_2_12(E).
 
-display_line_2([S|E]):-
+display_line_2_12(Board):-
   write('|                '),
-  display_elems_line_2(S,E).
-display_elems_line_2([S|E],T):-
+  display_elems_line_2_12(Board, 1).
+display_elems_line_2_12([S|E], Counter):-
+  Counter =< 3,
+  Counter1 is Counter+1,
   show_piece(S),
   write('    '),
-  display_elems_line_2(E, T).
-display_elems_line_2([], T):-
+  display_elems_line_2_12(E, Counter1).
+display_elems_line_2_12(E, 4):-
   write('             |'),
   nl,
   write('|                                               |'),
   nl,
-  display_line_3(T).
+  display_line_3_11(E).
 
-display_line_3([S|E]):-
+display_line_3_11(Board):-
   write('|                   '),
-  display_elems_line_3(S,E).
-display_elems_line_3([S|E],T):-
+  display_elems_line_3_11(Board, 1).
+display_elems_line_3_11([S|E],Counter):-
+  Counter =< 2,
+  Counter1 is Counter+1,
   show_piece(S),
   write('    '),
-  display_elems_line_3(E, T).
-display_elems_line_3([], T):-
+  display_elems_line_3_11(E, Counter1).
+display_elems_line_3_11(E, 3):-
     write('                |'),
     nl,
-    display_line_4(T).
+    display_line_4_10(E).
 
-display_line_4([S|E]):-
+display_line_4_10(Board):-
   write('|   '),
-  display_elems_line_4(S,E).
-display_elems_line_4([S|E],T):-
+  display_elems_line_4_10(Board, 1).
+display_elems_line_4_10([S|E], 1):-
   show_piece(S),
-  display_elems_line_4_1(E, T).
-display_elems_line_4_1([S|E], T):-
+  display_elems_line_4_10_1(E).
+display_elems_line_4_10_1([S|E]):-
   write('                                     '),
   show_piece(S),
-  display_elems_line_4_1(E, T).
-display_elems_line_4_1([], T):-
+  display_elems_line_4_10_1(E, 1).
+display_elems_line_4_10_1(E, 1):-
   write('   |'),
   nl,
-  display_line_5(T).
+  display_line_5_9(E).
 
-display_line_5([S|E]):-
+display_line_5_9(Board):-
   write('|        '),
-  display_elems_line_5(S,E).
-display_elems_line_5([S|E],T):-
+  display_elems_line_5_9(Board, 0).
+display_elems_line_5_9([S|E], 0):-
   show_piece(S),
-  display_elems_line_5_1(E, T).
-display_elems_line_5_1([S|E], T):-
+  display_elems_line_5_9_1(E, 1).
+display_elems_line_5_9_1([S|E], Counter):-
+  Counter =< 2,
+  Counter1 is Counter+1,
   write('            '),
   show_piece(S),
-  display_elems_line_5_1(E, T).
-display_elems_line_5_1([], T):-
+  display_elems_line_5_9_1(E, Counter1).
+display_elems_line_5_9_1(E, 3):-
   write('         |'),
   nl,
-  display_line_6(T).
+  display_line_6_8(E).
 
-display_line_6([S|E]):-
+display_line_6_8(Board):-
   write('|   '),
-  display_elems_line_6(S,E).
-display_elems_line_6([S|E],T):-
+  display_elems_line_6_8(Board, 1).
+display_elems_line_6_8([S|E], 1):-
   show_piece(S),
   write('         '),
-  display_elems_line_6_1(E, T).
-display_elems_line_6_1([S|E],T):-
+  display_elems_line_6_8_1(E).
+display_elems_line_6_8_1([S|E]):-
   show_piece(S),
   write('               '),
-  display_elems_line_6_2(E, T).
-display_elems_line_6_2([S|E], T):-
+  display_elems_line_6_8_2(E).
+display_elems_line_6_8_2([S|E]):-
   show_piece(S),
   write('         '),
-  display_elems_line_6_3(E, T).
-display_elems_line_6_3([S|E], T):-
+  display_elems_line_6_8_3(E).
+display_elems_line_6_8_3([S|E]):-
   show_piece(S),
-  display_elems_line_6(E, T).
-display_elems_line_6([], T):-
+  display_elems_line_6_8(E, 2).
+display_elems_line_6_8(E, 2):-
   write('   |'),
   nl,
-  display_line_7(T).
+  display_line_7(E).
 
-display_line_7([S|E]):-
+display_line_7(Board):-
   write('|        '),
-  display_elems_line_7(S,E).
-display_elems_line_7([S|E],T):-
+  display_elems_line_7(Board,1).
+display_elems_line_7([S|E], Counter):-
+  Counter =< 2,
+  Counter1 is Counter+1,
   show_piece(S),
   write('        '),
-  display_elems_line_7_1(E, T).
-display_elems_line_7_1([S|E],T):-
+  display_elems_line_7_1(E, Counter1).
+display_elems_line_7_1([S|E], Counter):-
+  Counter =< 2,
   show_piece(S),
   write('  '),
-  display_elems_line_7_2(E, T).
-display_elems_line_7_2([S|E],T):-
+  display_elems_line_7_2(E, Counter).
+display_elems_line_7_2([S|E], Counter):-
   show_piece(S),
   write('  '),
-  display_elems_line_7(E, T).
-display_elems_line_7_2([], T):-
-  write('       |'),
-    nl,
-    display_line_8(T).
-
-display_line_8([S|E]):-
-  write('|   '),
-  display_elems_line_8(S,E).
-display_elems_line_8([S|E],T):-
+  display_elems_line_7(E, Counter).
+display_elems_line_7_1([S|E], 3):-
   show_piece(S),
-  write('         '),
-  display_elems_line_8_1(E, T).
-display_elems_line_8_1([S|E],T):-
-  show_piece(S),
-  write('               '),
-  display_elems_line_8_2(E, T).
-display_elems_line_8_2([S|E], T):-
-  show_piece(S),
-  write('         '),
-  display_elems_line_8_3(E, T).
-display_elems_line_8_3([S|E], T):-
-  show_piece(S),
-  display_elems_line_8(E, T).
-display_elems_line_8([], T):-
-  write('   |'),
-  nl,
-  display_line_9(T).
-
-display_line_9([S|E]):-
-  write('|        '),
-  display_elems_line_9(S,E).
-display_elems_line_9([S|E],T):-
-  show_piece(S),
-  display_elems_line_9_1(E, T).
-display_elems_line_9_1([S|E], T):-
-  write('            '),
-  show_piece(S),
-  display_elems_line_9_1(E, T).
-display_elems_line_9_1([], T):-
   write('         |'),
   nl,
-  display_line_10(T).
+  display_line_8(E).
 
-  display_line_10([S|E]):-
-    write('|   '),
-    display_elems_line_10(S,E).
-  display_elems_line_10([S|E],T):-
-    show_piece(S),
-    display_elems_line_10_1(E, T).
-  display_elems_line_10_1([S|E], T):-
-    write('                                     '),
-    show_piece(S),
-    display_elems_line_10_1(E, T).
-  display_elems_line_10_1([], T):-
-    write('   |'),
-    nl,
-    display_line_11(T).
+display_line_8(Board):-
+  write('|   '),
+  display_elems_line_8(Board, 1).
+display_elems_line_8([S|E], 1):-
+  show_piece(S),
+  write('         '),
+  display_elems_line_8_1(E).
+display_elems_line_8_1([S|E]):-
+  show_piece(S),
+  write('               '),
+  display_elems_line_8_2(E).
+display_elems_line_8_2([S|E]):-
+  show_piece(S),
+  write('         '),
+  display_elems_line_8_3(E).
+display_elems_line_8_3([S|E]):-
+  show_piece(S),
+  display_elems_line_8(E, 2).
+display_elems_line_8(E, 2):-
+  write('   |'),
+  nl,
+  display_line_9(E).
 
-display_line_11([S|E]):-
+display_line_9(Board):-
+  write('|        '),
+  display_elems_line_9(Board, 0).
+display_elems_line_9([S|E], 0):-
+  show_piece(S),
+  display_elems_line_9_1(E, 1).
+display_elems_line_9_1([S|E], Counter):-
+  Counter =< 2,
+  Counter1 is Counter+1,
+  write('            '),
+  show_piece(S),
+  display_elems_line_9_1(E, Counter1).
+display_elems_line_9_1(E, 3):-
+  write('         |'),
+  nl,
+  display_line_10(E).
+
+display_line_10(Board):-
+  write('|   '),
+  display_elems_line_10(Board, 1).
+display_elems_line_10([S|E], 1):-
+  show_piece(S),
+  display_elems_line_10_1(E).
+display_elems_line_10_1([S|E]):-
+  write('                                     '),
+  show_piece(S),
+  display_elems_line_10_1(E, 1).
+display_elems_line_10_1(E, 1):-
+  write('   |'),
+  nl,
+  display_line_11(E).
+
+display_line_11(Board):-
   write('|                   '),
-  display_elems_line_11(S,E).
-display_elems_line_11([S|E],T):-
+  display_elems_line_11(Board, 1).
+display_elems_line_11([S|E],Counter):-
+  Counter =< 2,
+  Counter1 is Counter+1,
   show_piece(S),
   write('    '),
-  display_elems_line_11(E, T).
-display_elems_line_11([], T):-
+  display_elems_line_11(E, Counter1).
+display_elems_line_11(E, 3):-
   write('                |'),
-    nl,
-    write('|                                               |'),
-    nl,
-    display_line_12(T).
+  nl,
+  display_line_12(E).
 
-display_line_12([S|E]):-
+display_line_12(Board):-
+  write('|                                               |'),
+  nl,
   write('|                '),
-  display_elems_line_12(S,E).
-display_elems_line_12([S|E],T):-
+  display_elems_line_12(Board, 1).
+display_elems_line_12([S|E], Counter):-
+  Counter =< 3,
+  Counter1 is Counter+1,
   show_piece(S),
   write('    '),
-  display_elems_line_12(E, T).
-display_elems_line_12([], T):-
+  display_elems_line_12(E, Counter1).
+display_elems_line_12(E, 4):-
   write('             |'),
-    nl,
-    write('|                                               |'),
-    nl,
-    display_line_13(T).
-
-display_line_13([S|E]):-
+  nl,
+  write('|                                               |'),
+  nl,
   write('|             '),
-  display_elems_line_13(S,E).
-display_elems_line_13([S|E],T):-
+  display_elems_line_13(E, 1).
+
+display_elems_line_13([S|E], Counter):-
+  Counter =< 4,
+  Counter1 is Counter+1,
   show_piece(S),
   write('    '),
-  display_elems_line_13(E, T).
-  display_elems_line_13([], T):-
-    write('          |'),
-    nl,
-    write('|                                               |'),
-    nl,
+  display_elems_line_13(E, Counter1).
+display_elems_line_13(E, 5):-
+  write('          |'),
+  nl,
+  write('|                                               |'),
+  nl,
+  display_line_end.
+
+display_line_end:-
     write('|_______________________________________________|').
