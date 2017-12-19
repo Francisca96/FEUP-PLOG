31b0cd4efc041c67b39f07373799908b4b4d82fc
Francisca96 <franciscapauperio@gmail.com>
2017-12-19 14:50:07 +0000

    constrain middle cells - with error

--- Proj. 2 - Bosnian Snake/BosnianSnake.pl
+++ Proj. 2 - Bosnian Snake/BosnianSnake.pl
@@ -1,11 +1,14 @@
 :- include('Constraints.pl').
 
 %puzzle(numero, dimensao, numeros de fora(linha numero), numeros de fora(coluna numero), numeros de dentro(linha coluna numero))
-puzzle(1, 6, [2-2, 5-1], [], [2-4-6, 5-3-6]).
+puzzle(1, 6, [2-2, 5-1], [], [5-2-6, 3-5-6]).
 
 %N é o número do puzzle
 main(N):-
   puzzle(N, Size, [ORow1-ONum1, ORow2-ONum2], [], [IRow1-ICol1-INum1, IRow2-ICol2-INum2]),
   define_board(Size, Board),
   constrain_init_final_cells(Board, Size),
+  constrain_middle_cells(Board, Size, IRow1, ICol1, INum1),
+  %constrain_middle_cells(Board, Size, IRow2, ICol2, INum2),
+  labeling([], Board),
   write(Board).
--- Proj. 2 - Bosnian Snake/Constraints.pl
+++ Proj. 2 - Bosnian Snake/Constraints.pl
@@ -10,3 +10,24 @@ constrain_init_final_cells(Board, Size):-
   Length is Size*Size,
   nth1(1, Board, 1),
   nth1(Length, Board, 1).
+
+constrain_middle_cells(Board, Size, IRow, ICol, INum):-
+  MiddlePos is (IRow-1)*Size+ICol,
+  Pos1 is MiddlePos-Size,
+  Pos2 is MiddlePos+Size,
+  Pos3 is MiddlePos-1,
+  Pos4 is MiddlePos+1,
+  Pos5 is Pos1-1,
+  Pos6 is Pos1+1,
+  Pos7 is Pos2-1,
+  Pos8 is Pos2+1,
+  setof(Cell, (nth1(Pos1, Board, Cell),
+                nth1(Pos2, Board, Cell),
+                nth1(Pos3, Board, Cell),
+                nth1(Pos4, Board, Cell),
+                nth1(Pos5, Board, Cell),
+                nth1(Pos6, Board, Cell),
+                nth1(Pos7, Board, Cell),
+                nth1(Pos8, Board, Cell)), AdjCells),
+  INum1 is 8-INum,
+  global_cardinality(AdjCells, [1-INum, 0-INum1]).
