ad7869daaed75a696d63876b11a2ac51f01e9620
trocatudo95 <pedromachado_95@sapo.pt>
2017-12-20 14:39:54 +0000

    horizontal contrains working

--- Proj. 2 - Bosnian Snake/BosnianSnake.pl
+++ Proj. 2 - Bosnian Snake/BosnianSnake.pl
@@ -6,10 +6,13 @@ puzzle(1, 6, [2-2, 5-1], [], [5-2-6, 3-5-6]).
 %N é o número do puzzle
 main(N):-
   puzzle(N, Size, HorizontalConstrains, [], [IRow1-ICol1-INum1, IRow2-ICol2-INum2]),
-  define_board(Size, Board),
-  constrain_init_final_cells(Board, Size),
-  constrain_middle_cells(Board, Size, IRow1, ICol1, INum1),
-  constrain_middle_cells(Board, Size, IRow2, ICol2, INum2),
-  constrain_horizontal_lines(Board,Size,HorizontalConstrains),
-  labeling([], Board),
-  write(Board).
+  define_board(Size, List),
+  constrain_init_final_cells(List, Size),
+  constrain_middle_cells(List, Size, IRow1, ICol1, INum1),
+  constrain_middle_cells(List, Size, IRow2, ICol2, INum2),
+
+
+  list_to_matrix(List,Size,Board),
+  %constrain_horizontal_lines(Board,HorizontalConstrains),
+  labeling([], List),
+  print(List).
--- Proj. 2 - Bosnian Snake/Constraints.pl
+++ Proj. 2 - Bosnian Snake/Constraints.pl
@@ -32,20 +32,28 @@ constrain_middle_cells(Board, Size, IRow, ICol, INum):-
   INum1 is 8-INum,
   global_cardinality(AdjCells, [1-INum, 0-INum1]).
 
-constrain_horizontal_lines(_,_,[]):-!.
-constrain_horizontal_lines(Board,Size,[Row-Num|T]):-
-  Pos1 is (Row-1)*Size,
-  Pos2 is Pos1-1,
-  Pos3 is Pos2-1,
-  Pos4 is Pos3-1,
-  Pos5 is Pos4-1,
-  Pos6 is Pos5-1,
-  setof(Cell, (nth1(Pos1, Board, Cell);
-                nth1(Pos2, Board, Cell);
-                nth1(Pos3, Board, Cell);
-                nth1(Pos4, Board, Cell);
-                nth1(Pos5, Board, Cell);
-                nth1(Pos6, Board, Cell)), AdjCells),
-                 Num1 is Size -Num,
-  global_cardinality(AdjCells,[1-Num,0-Num1]),
-  constrain_horizontal_lines(Board,Size,T).
+  list_to_matrix([], _,[]).
+
+    list_to_matrix(List, Size,[Row|Matrix]):-
+      list_to_matrix_row(List,Size,Row,Tail),
+      list_to_matrix(Tail,Size,Matrix).
+
+      list_to_matrix_row(Tail,0,[],Tail).
+
+      list_to_matrix_row([Item|List], Size, [Item|Row],Tail):-
+        NSize is Size-1,
+        list_to_matrix_row(List,NSize,Row,Tail).
+
+
+
+
+constrain_horizontal_lines(Board,[Row-Num|T]):-
+  nth1(Row,Board,Line),
+  sum(Line,#=,Num),
+  constrain_horizontal_lines(Board,T).
+
+  constrain_horizontal_lines(_,[]).
+
+
+
+constrain_horizontal_lines(_,[]).
