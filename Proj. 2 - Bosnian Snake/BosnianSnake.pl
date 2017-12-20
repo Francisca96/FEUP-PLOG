2a330d3aae031509683f29db5982911a7a3e4520
trocatudo95 <pedromachado_95@sapo.pt>
2017-12-20 01:33:26 +0000

    recursive way doesnt work

--- Proj. 2 - Bosnian Snake/BosnianSnake.pl
+++ Proj. 2 - Bosnian Snake/BosnianSnake.pl
@@ -5,11 +5,11 @@ puzzle(1, 6, [2-2, 5-1], [], [5-2-6, 3-5-6]).
 
 %N é o número do puzzle
 main(N):-
-  puzzle(N, Size, Horizontal_Constrains, [], [IRow1-ICol1-INum1, IRow2-ICol2-INum2]),
+  puzzle(N, Size, HorizontalConstrains, [], [IRow1-ICol1-INum1, IRow2-ICol2-INum2]),
   define_board(Size, Board),
   constrain_init_final_cells(Board, Size),
   constrain_middle_cells(Board, Size, IRow1, ICol1, INum1),
   constrain_middle_cells(Board, Size, IRow2, ICol2, INum2),
-  constrain_horizontal_lines(Board,Size,HorizontalConstrains), 
+  constrain_horizontal_lines(Board,Size,HorizontalConstrains),
   labeling([], Board),
   write(Board).
--- Proj. 2 - Bosnian Snake/Constraints.pl
+++ Proj. 2 - Bosnian Snake/Constraints.pl
@@ -32,11 +32,20 @@ constrain_middle_cells(Board, Size, IRow, ICol, INum):-
   INum1 is 8-INum,
   global_cardinality(AdjCells, [1-INum, 0-INum1]).
 
-constrain_horizontal_lines(_,_,[]).
+constrain_horizontal_lines(_,_,[]):-!.
 constrain_horizontal_lines(Board,Size,[Row-Num|T]):-
-  N #>=0, N #<=Size,
-  Pos is (Row-1) *Size + N,
-  setof(Cell,(nth1(Pos,Board,Cell)),List),
-  Num1 is Size -Num,
-  global_cardinality(List,[1-Num,0-Num1]),
+  Pos1 is (Row-1)*Size,
+  Pos2 is Pos1-1,
+  Pos3 is Pos2-1,
+  Pos4 is Pos3-1,
+  Pos5 is Pos4-1,
+  Pos6 is Pos5-1,
+  setof(Cell, (nth1(Pos1, Board, Cell);
+                nth1(Pos2, Board, Cell);
+                nth1(Pos3, Board, Cell);
+                nth1(Pos4, Board, Cell);
+                nth1(Pos5, Board, Cell);
+                nth1(Pos6, Board, Cell)), AdjCells),
+                 Num1 is Size -Num,
+  global_cardinality(AdjCells,[1-Num,0-Num1]),
   constrain_horizontal_lines(Board,Size,T).
