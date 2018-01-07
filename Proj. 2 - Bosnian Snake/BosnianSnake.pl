facf86e491b8dd1f753e44b460350f17d2ae0a60
trocatudo95 <pedromachado_95@sapo.pt>
2018-01-07 20:27:15 +0000

    puzzles to show

--- Proj. 2 - Bosnian Snake/BosnianSnake.pl
+++ Proj. 2 - Bosnian Snake/BosnianSnake.pl
@@ -11,10 +11,10 @@
 
 %puzzle(numero, dimensao, numeros de fora(linha numero), numeros de fora(coluna numero), numeros de dentro(linha coluna numero))
 puzzle(1, 6, [5-1, 2-2], [], [5-2-6, 3-5-6]).
-puzzle(2, 8, [2-2, 5-1], [7-3, 4-5], [2-2-4, 7-6-5]).
-puzzle(3, 13, [2-4, 5-1,6-1], [7-3], [2-2-4, 7-6-5, 10-10-3]).
-puzzle(4, 6, [4-3],[4-2],[3-3-5]).
-puzzle(5,10, [7-3,8-3],[3-6],[4-8-6,6-3-2]).
+puzzle(2, 7, [4-2], [], [3-2-3, 4-3-1]).
+puzzle(4, 10, [8-3],[3-6],[8-4-2,8-8-1]).
+puzzle(3, 8, [5-2,6-2],[2-4],[2-4-5]).
+puzzle(5,15, [7-9,12-7],[2-13],[11-3-4]).
 
 solve_generated_board(Size,MiddleRestricions,HorizontalRestrictions,VerticalRestrictions):-
   repeat,
